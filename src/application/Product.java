b39ba1bca3e25f692a2482e45e282fc9ad3c9a81
FABIO SANTOS LOUZADA JUNIOR <fabiobr876@gmail.com>
2026-05-22 22:30:48 -0300

    Primeira Estrutura do Projeto

---
+++ src/application/Product.java
@@ -0,0 +1,41 @@
+package application;
+
+public class Product {
+    private String name;
+    private String category;
+    private Double price;
+    
+    public Product (){
+
+    }
+
+    public Product(String category, Double price, String name) {
+        this.category = category;
+        this.price = price;
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+}
